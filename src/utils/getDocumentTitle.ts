6e86bc8f6428bf335f68711830249af516e306a5
bambiru <dj70083798@gmail.com>
2024-04-12 19:22:42 +0900

    [♻️Refactor]: src/utils ts로 변경
    utils에 있는 js -> ts로 변경

--- src/utils/getDocumentTitle.ts
+++ src/utils/getDocumentTitle.ts
@@ -1,5 +1,8 @@
 const SITE_TITLE = '카페 어때';
 
-export default function getDocumentTitle(pageTitle, siteTitle = SITE_TITLE) {
+export default function getDocumentTitle(
+  pageTitle: string,
+  siteTitle = SITE_TITLE
+): string {
   return !pageTitle ? siteTitle : `${pageTitle} ← ${siteTitle}`;
 }
--- src/utils/pocketbase.ts
+++ src/utils/pocketbase.ts
@@ -2,7 +2,11 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase(import.meta.env.VITE_PB_URL);
 pb.autoCancellation(false);
-export const pbImg = (collectionId, recordId, mainImage) => {
+export const pbImg = (
+  collectionId: string,
+  recordId: string,
+  mainImage: string
+) => {
   return `${import.meta.env.VITE_PB_URL}/api/files/${collectionId}/${recordId}/${mainImage}`;
 };
 
